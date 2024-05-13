ec15a8dfe4bba42a4451edeed7b2e248b384a1ed
HotWasabiSushi <76900409+CallMeWasabi@users.noreply.github.com>
2024-05-13 18:44:34 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Button } from "@nextui-org/react"
+
+function App() {
+
+  return (
+    <>
+      <div className="flex gap-2">
+        <Button color="primary" variant="solid">Hello Larmar!</Button>
+        <Button color="primary" variant="bordered">Hello Larmar!</Button>
+        <Button color="primary" variant="faded">Hello Larmar!</Button>
+        <Button color="primary" variant="flat">Hello Larmar!</Button>
+        <Button color="primary" variant="ghost">Hello Larmar!</Button>
+        <Button color="primary" variant="light">Hello Larmar!</Button>
+        <Button color="primary" variant="shadow">Hello Larmar!</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
